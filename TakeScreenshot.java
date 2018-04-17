e4e021602f0aaf4e5d38a9a593e73250909b3445
laxmotive <38479220+laxmotive@users.noreply.github.com>
2018-04-17 16:17:17 -0700

    Add files via upload

---
+++ TakeScreenshot.java
@@ -0,0 +1,65 @@
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TakeScreenshot {
+
+    @Test
+
+    public void testGuru99TakeScreenShot() throws Exception{
+
+		WebDriver driver ;
+		// declaration and instantiation of objects/variables
+				System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Drivers\\geckodriver\\geckodriver.exe");
+				System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Drivers\\chromedriver\\chromedriver.exe");
+    	driver = new FirefoxDriver();
+
+        //goto url
+
+        driver.get("http://demo.guru99.com/V4/");
+
+        //Call take screenshot function
+
+        this.takeSnapShot(driver, "C:\\Users\\Lpullethu\\Desktop\\screenshot\\test.png") ;     
+
+    }
+
+    /**
+
+     * This function will take screenshot
+
+     * @param webdriver
+
+     * @param fileWithPath
+
+     * @throws Exception
+
+     */
+
+    public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception{
+
+        //Convert web driver object to TakeScreenshot
+
+        TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+
+        //Call getScreenshotAs method to create image file
+
+                File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+            //Move image file to new destination
+
+                File DestFile=new File(fileWithPath);
+
+                //Copy file at destination
+
+                FileUtils.copyFile(SrcFile, DestFile);
+
+    }
+
+}
